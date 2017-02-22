2b5a12dc999fc8117cfb6e1ce31dfd11d41c03db
Vittor <vittorfp@github.com>
2017-02-22 11:11:18 -0300

    Novos scripts adicionados, alguns ainda nao estao funcionais.

--- Powerspectrum.m
+++ Powerspectrum.m
@@ -1,31 +1,31 @@
 %% Power spectrum 
 
 Data=R42HIPO1_1khz;
-%(13783000:13793000)
-M=Data(14842000:14852000);
+%(13783000:13793000)(14842000:14852000)
+M=Data(2500000:2510000);
 
 t1=(1:1:length(M))/1000;
 
 params.Fs=1000; % sampling frequency
 params.fpass=[1 50]; % band of frequencies to be kept
-params. tapers = [5 9]; % taper parameters
+params. tapers = [10 10]; % taper parameters
 params.pad=0; % pad factor for fft
 params.err=[0 0.05];
 params.trialave = 0;
 
 
-        [S,t,f]=mtspecgramc(M',[1 0.001],params);
+        [S,t,f]=mtspecgramc(M',[1.5 0.001],params);
         
         
 params2.Fs=1000; % sampling frequency
-params2.fpass=[120 180]; % band of frequencies to be kept
+params2.fpass=[90 140]; % band of frequencies to be kept
 params2. tapers=[5 9]; % taper parameters
 params2.pad=0; % pad factor for fft
 params2.err=[0 0.05];
 params2.trialave=0;
 
 
-        [S2,t2,f2]=mtspecgramc(M',[0.5 0.001],params2);
+        [S2,t2,f2]=mtspecgramc(M',[1.5 0.001],params2);
         
 subplot(3,1,1);
 plot(t1,M);
---
+++ divisao.m
@@ -0,0 +1,19 @@
+dado  = R42HIPO1_1khz;
+
+m= floor(length(dado)/10000) ; % número de épocas (de 10s)
+
+ n=10000; % dado em volts
+
+ Epocas=zeros(m,n);
+
+ for i=1:m
+
+     a = 1 + ( i - 1 ) * n;
+
+     b = i*n;
+
+     Epocas(i,:)=dado(a:b);
+
+ end
+
+ clear m n a b dado
