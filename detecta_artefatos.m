fb2cc35cb24a2162a21a96eac4d6e6fd2d976d5f
vittorfp <vittorfp@github.com>
2017-02-23 17:49:27 -0300

    Versão nao testada do script para detectar artefatos nas series temporais

---
+++ detecta_artefatos.m
@@ -0,0 +1,15 @@
+
+
+
+dado = Epocas;  //O script recebe os dados ja separados em epocas
+treshold =  //Definido empiricamente
+len = length(Epocas);
+
+for i=1:len 
+
+	if ( max(Epocas(i,:)) > treshold )
+		Epocas(i,:) = NaN;
+	end
+
+end
+
